6c3fb0128f7c51e7f93d938daee0ca3477380b9f
Eric <erjohnson.pdx@gmail.com>
2014-08-27 14:29:12 -0700

    Run tickets migration

--- db/migrate/20140827212728_create_tickets.rb
+++ db/migrate/20140827212728_create_tickets.rb
@@ -1,6 +1,11 @@
 class CreateTickets < ActiveRecord::Migration
   def change
     create_table :tickets do |t|
+      t.string :student
+      t.string :question
+      t.integer :project_id
+      t.boolean :open
+      t.timestamps
     end
   end
 end
