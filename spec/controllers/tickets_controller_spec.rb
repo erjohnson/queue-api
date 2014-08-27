16f8c92ce9ec9f359919125d1a184f3c1b492cb7
Eric <erjohnson.pdx@gmail.com>
2014-08-27 15:26:48 -0700

    Create request helper

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 describe TicketsController do
   it { should route(:get, '/tickets').to(action: :index) }
+
+  describe 'GET #index' do
+    before { get :index }
+    it { should respond_with(200) }
+  end
 end
