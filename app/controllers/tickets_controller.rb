9f20aa9854ebe6e3cbf633a138f00665375973c0
Eric <erjohnson.pdx@gmail.com>
2014-08-27 15:51:57 -0700

    Add tickets#create handler

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,4 +2,9 @@ class TicketsController < ApplicationController
   def index
     render json: Ticket.all
   end
+
+  def create
+    @ticket = Ticket.new(params[:ticket])
+    @ticket.save
+  end
 end
