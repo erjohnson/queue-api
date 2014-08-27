218e1d33d2514709ff7a28b53cb7104d53bff38d
Eric <erjohnson.pdx@gmail.com>
2014-08-27 16:00:13 -0700

    Update tickets controller spec

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 describe TicketsController do
   it { should route(:get, '/tickets').to(action: :index) }
+
   it { should route(:post, '/tickets').to(action: :create) }
 
   describe 'GET #index' do
     before { get :index }
+
     it { should respond_with(200) }
   end
 
   describe 'POST #create' do
     before { post :create }
+
     it { should respond_with(200) }
   end
 end
