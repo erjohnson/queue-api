466e4f6ee48583b93834db32ddf8aa2ba8b80f0c
Eric <erjohnson.pdx@gmail.com>
2014-08-27 16:01:38 -0700

    Add tickets#show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   match('tickets', {:via => :get, :to => 'tickets#index'})
   match('tickets', {:via => :post, :to => 'tickets#create'})
+  match('tickets/:id', {:via => :get, :to => 'tickets#show'})
 end
