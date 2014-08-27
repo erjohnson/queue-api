e9b3bd190d7ed544c4362fff682ac87efacc7155
Eric <erjohnson.pdx@gmail.com>
2014-08-27 14:40:05 -0700

    Add validation on Ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,7 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
+
+  validate :student, :presence => true
+  validate :question, :presence => true
+  validate :project_id, :presence => true
 end
